e493ec90e1d0cb08ea9c10df4d3bf44051088410
Dale Eman <deman001@sledge.cs.ucr.edu>
2023-01-19 16:56:48 -0800

    implemented sleep

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Need an argument. Usage: sleep [num]\n");
+        exit(0);
+    }
+
+    int time = atoi(argv[1]);
+    sleep(time);
+    exit(0);
+    return 0;
+}
